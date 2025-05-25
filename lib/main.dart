a54265129e07300b30e94e74fd2c9d8a22c88b50
wlstn5555 <wlstn56789@gmail.com>
2025-05-26 02:09:08 +0900

    기능구현 안되어있는 거 빼고 나머지 페이지 탑바 통일

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Pretendard',
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/', // 시작화면
+      initialRoute: '/landing', // 시작화면
       routes: {
         '/': (context) => LoginScreen(),      // 첫 화면
         '/login': (context) => LoginScreen(), // 로그인 화면
