d40884b95570f4a1275525210a77dd517005cb5d
Your Name <your.email@example.com>
2025-09-22 21:12:50 +0900

    1. 삭제버튼 2. 삭제 처리 완료

---
+++ lib/features/recording/repository/delete_recording.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import '../../../ip/ip_address.dart';
+import '../model/recording.dart';
+
+Future<void> deleteRecording(String id) async {
+  // SharedPreferences에서 accessToken 불러오기
+  final prefs = await SharedPreferences.getInstance();
+  final accessToken = prefs.getString('accessToken');
+
+  if (accessToken == null) {
+    throw Exception('Access token이 없습니다.');
+  }
+
+  // API 호출
+  final url = Uri.parse('http://$ipAddress:8080/api/records/$id');
+  final response = await http.delete(
+    url,
+    headers: {
+      'Authorization': 'Bearer $accessToken',
+      'Content-Type': 'application/json',
+    },
+  );
+
+  if (response.statusCode != 200) {
+    throw Exception('녹음 삭제 실패: ${response.statusCode}');
+  }
+}
